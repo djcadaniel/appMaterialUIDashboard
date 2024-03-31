9650a23c19ece7420f88c2b44112af464c657cd0
djcadaniel <djcadaniel@gmail.com>
2024-03-31 09:15:25 -0500

    add app material

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,44 @@
+import { Button, Grid, TextField, Typography, Link } from "@mui/material"
+import { AuthLayout } from "../layout/AuthLayout"
+import { Google } from "@mui/icons-material"
+import { Link as RouterLink } from "react-router-dom"
+
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Register'>
+      <form>
+        <Grid container>
+          <Grid item xs={ 12 } md={ 6 } sx={{ mt:2 }} >
+            <TextField 
+              label='Nombre completo'
+              type='text'
+              placeholder="Nombre completo"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={ 12 } md={ 6 } sx={{ mt:2 }}>
+            <TextField 
+              label='Contraseña'
+              type='password'
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid>
+          <Grid container spacing={ 2 } sx={{ mb:2, mt:1 }}>
+            <Grid item xs={12} sm={12}>
+              <Button variant='contained' fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr:1 }}>¿Ya tienes cuenta?</Typography>
+            <Link component={ RouterLink } color='inherit' to='/auth/login'>
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,27 @@
-import { Typography } from "@mui/material"
-import { MailOutline } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../view"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae tempore aut ullam, alias obcaecati sunt nisi cumque saepe, beatae dolore facere qui? Qui odit repudiandae a consequuntur! Sed, magnam explicabo!</Typography> */}
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+      <IconButton
+        size='large'
+        sx={{
+          color:'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity:0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize:30 }}/>
+      </IconButton>
+    </JournalLayout>
   )
 }
